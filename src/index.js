2dac456738f695843d0beebd20d94098ff4d4f95
Daniel Tafese <dtafese@lititzmutual.com>
2018-12-31 15:00:51 -0500

    commit

---
+++ src/index.js
@@ -0,0 +1,12 @@
+// let's go!
+import React from 'react';
+import  { render } from 'react-dom';
+
+import Router from "./components/Router";
+import  './css/style.css'
+import  'bootstrap/dist/css/bootstrap.min.css'
+
+
+render(<Router />,document.querySelector('#main'));
+// render(<StorePicker />,document.querySelector('#main'));
+
